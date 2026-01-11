28fe61697c0a4d4c7c21bf3b3b604bdf6795409c
Ramchandra Khadka <khadkaramchandra981@gmail.com>
2026-01-11 14:28:09 +0545

    include router in main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from inventory-managemnt!")
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from starlette.requests import Request
+from app.database import engine, Base
+from app.api import items, orders
 
+Base.metadata.create_all(bind=engine)
 
-if __name__ == "__main__":
-    main()
+app = FastAPI(title="Inventory & Orders Managemnt  API")
+
+app.include_router(items.router)
+
+app.include_router(orders.router)
+
+# Global fallback error handler for uncaught exceptions
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+	return JSONResponse(status_code=500, content={"detail": "Internal server error"})
